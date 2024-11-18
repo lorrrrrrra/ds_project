257986ad28d102aec2ee88dc3e8bd630a81de19f
Laura Gaddum <gaddula@web.de>
2024-11-18 17:21:56 +0100

    new sidebar on the right for details, contains opening and closing mechanism

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-function toggleSidebar() {
+function toggleSidebar(instruction) {
   const sidebar = document.querySelector('.sidebar');
   const arrow = document.querySelector('.toggle-arrow');
   const content = document.querySelector('.dashboard-filter');
   const burgerMenu = document.getElementById('burger-menu');
 
-  if (sidebar.style.width === '50px') {
+  if (instruction == 'open') {
       // Expand the sidebar
       sidebar.style.width = '20%';
       content.classList.remove('hidden'); // Inhalt wieder anzeigen
       burgerMenu.style.display = 'none'; // Burger-Menü ausblenden
-  } else {
+  } else if (instruction == 'close') { 
       // Collapse the sidebar
       sidebar.style.width = '50px';
       content.classList.add('hidden'); // Inhalt verstecken
       burgerMenu.style.display = 'block'; // Burger-Menü anzeigen
   }
+}
+
+
+function open_details() {
+  const sidebar = document.querySelector('.details-sidebar');
+  
+  if (!sidebar.classList.contains('open')) {
+      sidebar.classList.add('open'); // Sidebar einblenden
+      toggleSidebar('close');
+  }
+}
+
+
+function close_details() {
+  const sidebar = document.querySelector('.details-sidebar');
+
+  if (sidebar.classList.contains('open')) {
+    sidebar.classList.remove('open'); // Sidebar ausblenden
+}
 }

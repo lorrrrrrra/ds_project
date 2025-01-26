f6fcbb641a1e279f9eeb49e804ade5f89f179637
Laura Gaddum <gaddula@web.de>
2025-01-26 15:31:31 +0100

    function delete all filters

--- static/script.js
+++ static/script.js
@@ -402,6 +402,14 @@ function handleMarkerClick(markerId) {
 }
 
 
+
+
+
+
+
+
+
+
 // all things filter
 function setRating(rating, category) {
 
@@ -430,32 +438,32 @@ function setRating(rating, category) {
     updateStars(0, category);
     filters[category] = 0;
   }
-  
-
-  // for (let i = 1; i <= 5; i++) {
-  //     const star = document.getElementById(`star-${i}-filter-${category}`);
-      
-  //     if (rating == 0) {
-  //       star.src = "static/graphics/leerer_stern.png";
-  //     } else if (rating > 0 && i <= rating) {
-  //         if (i === rating) {
-  //             // Der Stern, auf den der Benutzer geklickt hat, kann entweder halb oder ganz gefüllt sein
-  //             star.src = "static/graphics/voller_stern.png"; // Ersetze durch den vollen Stern
-  //         } else {
-  //             // Alle Sterne vor dem angeklickten werden ganz gefüllt
-  //             star.src = "static/graphics/voller_stern.png";
-  //         }
-  //     } else {
-  //         // Alle Sterne nach dem angeklickten bleiben leer
-  //         star.src = "static/graphics/leerer_stern.png";
-  //     }
-
-  //     filters[category] = rating; 
-  // }
 }
 
 
+function delete_all_filters() {
+  const updateStars = (group) => {
+    const stars = [
+      document.getElementById(`star-1-filter-${group}`),
+      document.getElementById(`star-2-filter-${group}`),
+      document.getElementById(`star-3-filter-${group}`),
+      document.getElementById(`star-4-filter-${group}`),
+      document.getElementById(`star-5-filter-${group}`),
+    ];
 
+    stars.forEach((star, index) => {
+        star.src = "static/graphics/leerer_stern.png"; // Leerer Stern
+      });
+
+    filters[category] = 0;
+    
+  };
+
+  updateStars("general");
+  updateStars("food");
+  updateStars("service");
+  updateStars("atmosphere");
+}
 
 
 

e8b92213e3fce49ac3c5fe01f8c202e2b1be4cb0
Laura Gaddum <gaddula@web.de>
2025-01-30 15:35:05 +0100

    list not df

--- app.py
+++ app.py
@@ -239,9 +239,8 @@ def get_price_data_graph(restaurant_id):
 
         price_range_data = [row['dining_price_range'] for row in price_range_data if row['dining_price_range'] is not None]
         print(f"{price_range_data}")
-        price_range_df = pd.DataFrame(price_range_data, columns=["dining_price_range"])
 
-        result = count_price_ranges_with_all(price_range_df, all_price_ranges)
+        result = count_price_ranges_with_all(price_range_data, all_price_ranges)
 
         
         # Gebe die Informationen als JSON zurück
